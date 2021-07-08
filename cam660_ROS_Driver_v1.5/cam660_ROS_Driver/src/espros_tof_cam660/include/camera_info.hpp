d1936210b96fcae605226980ce941c012aa54dc9
Bruce Liu <mxl592@case.edu>
2021-07-08 13:34:10 -0400

    Add files via upload
    OMS files for ROS launcher

---
+++ cam660_ROS_Driver_v1.5/cam660_ROS_Driver/src/espros_tof_cam660/include/camera_info.hpp
@@ -0,0 +1,18 @@
+#ifndef __ESPROS_CAMERAINFO_H__
+#define __ESPROS_CAMERAINFO_H__
+
+#include <cstdint>
+
+namespace Espros {
+
+struct CameraInfo {
+  uint16_t frame_id;
+  uint16_t width;
+  uint16_t height;
+  uint16_t roiX0;
+  uint16_t roiY0;
+  uint16_t roiX1;
+  uint16_t roiY1;
+};
+}
+#endif // __ESPROS_CAMERAINFO_H__
---
+++ cam660_ROS_Driver_v1.5/cam660_ROS_Driver/src/espros_tof_cam660/include/udp_server.hpp
@@ -0,0 +1,34 @@
+#ifndef __ESPROS_UDPSERVER_H__
+#define __ESPROS_UDPSERVER_H__
+
+#include <boost/asio.hpp>
+#include <boost/signals2.hpp>
+
+using boost::asio::ip::udp;
+
+namespace Espros {
+
+    typedef std::vector<uint8_t> Packet;
+
+    class UdpServer {
+      static const int PORT = 45454;
+      static const int RECV_BUFF_SIZE = 2048;
+
+    public:
+      UdpServer(boost::asio::io_service &);
+      ~UdpServer();
+
+      boost::signals2::connection subscribe(std::function<void(Packet &)>);
+
+    private:
+      udp::socket socket;
+      udp::endpoint remoteEndpoint;
+      Packet recvBuffer;
+
+      boost::signals2::signal<void (Packet &)> dataReady;
+
+      void startReceive();
+      void handleReceive(const boost::system::error_code &, std::size_t);
+    };
+}
+#endif
